49645f81d7dae40fb1bb8bff10e919156d5f8eb3
ymauri <ymauri@outlook.com>
2020-11-15 23:59:42 +0100

    Delete unnecessary imported libraries

--- app/Http/Controllers/RuleController.php
+++ app/Http/Controllers/RuleController.php
@@ -3,16 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
-use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 use App\Models\Rule;
-use Carbon\Carbon;
 
 class RuleController extends Controller
 {
